7e9c00b5335e534e5ba7936ec4ba61d7677f09cd
fooody <paki_jan@yahoo.com>
2019-09-13 16:38:53 +0500

    React navigation

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Aboutstyle.css';
+
+const About = () => {
+    return (
+        <div className="aboutstyle">
+             <h1>About US Page</h1>
+        </div>
+    )
+}
+export default About;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import Slider from './components/slider';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import * as serviceWorker from './serviceWorker';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<slider />, document.getElementById('root'));
+
+
+serviceWorker.unregister();
